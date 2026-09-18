183b8519e30fc08c20e8af16e6437ec582f39307
giocassini <giovannacarvalho510@gmail.com>
2026-09-18 15:23:07 -0300

    Add basic arithmetic operations and calculations

---
+++ number.js
@@ -0,0 +1,51 @@
+
+// Exercício 1 - Crie dois números e exiba a soma.
+let soma = 10 + 5;
+console.log(soma);
+
+//Exercício 2 - Calcule a subtração.
+let subtração = 21-6;
+console.log(subtração);
+
+//Exercício 3 - Calcule a multiplicação.
+let multiplicação = 4 * 3;
+console.log(multiplicação);
+
+//Exercício 4 - Calcule a divisão.
+let divisão = 34 / 2;
+console.log(divisão);
+
+//Exercício 5 - Calcule a média de dois números.
+let nota1 = 8.0 ;
+let nota2 = 7.5 ;
+let nota3 = 9.0 ;
+let media = ( 8.0 / 7.5 / 9.0);
+console.log("A média do aluno é" + média);
+
+//Exercício 6 - Calcule o dobro de um número.
+let numero = 4 ;
+let dobro = numero * 2; 
+console.log("O dobro de " + numero  + dobro );
+
+//Exercício 7 - Calcule o resto da divisão.
+let dividendo = 11 ;
+let divisor = 3 ;
+let dividendodivisor = ( 11 / 3 );
+console.log("O resto da divisão é" + resto ) ; 
+
+//Exercício 8 - Crie um preço e um desconto.
+let preçooriginal = 150.00 ;
+let descontoporcentagem = 6 ; 
+let valordesconto = (preçooriginal * descontoporcentagem ) / 100  ;
+let preçoFinal = preçoOriginal - valordesconto ;
+console.log("valor do desconto: $ " + valordesconto ) ; 
+console.log("preço Final do desconto:R$" + preçoFinal.toFixed(2)) ; 
+
+//Exercício 9 - Converta idade em meses.
+idade = 15 ;
+meses = 6 ;
+let converteIdadesMeses = (15 / 6 ) ;
+//Exercício 10 - Calcule o quadrado de um número 3
+const numero = 3;
+const quadrado = numero ** 2; 
+console.log(`O quadrado de ${numero} é ${quadrado}`);
