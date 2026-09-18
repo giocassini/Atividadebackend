e9ccc0d9dd4a9e7e5b84d824ca591d262b9ead5c
giocassini <giovannacarvalho510@gmail.com>
2026-09-18 15:20:22 -0300

    Create string.js

---
+++ string.js
@@ -0,0 +1,52 @@
+
+// Exercício 1 - Crie uma variável nome com seu nome e exiba no console. 
+let nome = "Giovanna" ;
+console.log(typeof nome);
+
+// Exercício 2 - Crie uma variável sobrenome e mostre nome + sobrenome. 
+let nomeCompleto = "Giovanna Carvalho Cassini de OLiveira";
+console.log(nome+ " "+ sobrenome);
+
+// Exercício 3 - Crie uma frase usando Template String. 
+let frase = "Meu nome é ${nome} ${Sobrenome}! ";
+console.log(frase)
+
+// Exercício 4 - Guarde o nome de uma escola e exiba.
+
+let escola = "Sesi";
+console.log(escola);
+
+// Exercício 5 - Crie duas strings e junte em uma frase.
+
+let palavra1 = "Boa";
+let palavra2 = "Tarde";
+let fraseFinal = (palavra1 + " " +palavra2);
+console.log(fraseFinal);
+
+// Exercício 6 - Exiba uma mensagem de boas-vindas.
+ 
+let mensagemBoasVindas ="Bem vindo, ${seuNome}";
+console.log=(mensagemBoasVindas);
+
+// Exercício 7 - Mostre o tamanho de uma string.
+
+let seuSistemaFoihackeadolength = "Zerou!"
+console.log(seuSistemaFoihackeado.length);
+ 
+let regra = "não pode celular"
+console.log(regratoUpperCase());
+
+// Exercício 8 - Transforme um texto em letras maiúsculas.
+let textoMaiusculas = "Ontem fui a uma sorveteria e comi sorvete de flocos";
+textoMaiusculas = textoMaiusculas.toUpperCase(); 
+console.log(textoMaiusculas);
+
+// Exercício 9 - Transforme um texto em letras minúsculas.
+let textoMinuscula = "Tirei 9,0 na prova de matemática";
+let resultadoMinusculo = textoMinuscula.toLowerCase(); 
+console.log(resultadoMinusculo);
+
+
+//Exercício 10 - Crie uma frase com nome e cidade.
+let nomeCidade = "Eu fui para Pernambuco "
+console.log(nomeCidade); 
