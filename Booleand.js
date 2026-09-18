f9654fd5d8896120434f7f35a1dd1fb06e9192e0
giocassini <giovannacarvalho510@gmail.com>
2026-09-18 15:25:58 -0300

    Add Booleand.js with boolean operations examples

---
+++ Booleand.js
@@ -0,0 +1,47 @@
+// 1)
+let ativo = true;
+console.log (ativo);
+
+// 2)
+let numeroMaior = 15;
+console.log (numeroMaior > 10);
+ numeroMaior = 8;
+console.log (numeroMaior > 10);
+
+// 3)
+let idade = 54;
+console.log (idade >= 18);
+idade = 10;
+console.log (idade >= 18);
+
+// 4)
+let numeroA = 10; 
+let numeroB = 10;
+ console.log (numeroA === numeroB);
+
+//  5)
+console.log (numeroA !== numeroB);
+
+// 6)
+let numero = 7;
+console.log (numero % 2 === 0);
+
+// 7)
+let temIdade = true; 
+let temDocumento = true; 
+console.log(temIdade && temDocumento);
+
+// 8)
+let temIngresso = false; 
+let estaConvidado = true; 
+console.log(temIngresso || estaConvidado);
+
+// 9)
+ idade = 16;
+let podeDirigir = idade >= 18 ;
+console.log (!podeDirigir);
+
+// 10)
+let textoNumero = "10";
+let numeroValor = 10;
+console.log (textoNumero === numeroValor)
